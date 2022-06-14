6c3b9834c8d9056dce431e8aad69c34229b0a294
supersyz <362871010@qq.com>
2022-06-14 10:51:22 +0800

    '.'

--- compare_speed.py
+++ compare_speed.py
@@ -1,10 +1,12 @@
 import torch
 import models
 from time import time
+
 img = torch.randn(1, 3, 256, 256)
 net = models.MobileViT_S()
 img = img.cuda()
 net = net.cuda()
+net = net.eval()
 for i in range(20):
     out = net(img)
 tik = time()
