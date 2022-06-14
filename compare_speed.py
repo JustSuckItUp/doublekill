f374488bcf87b4b9f7e164e1a7fd5bc78b36d7b5
supersyz <362871010@qq.com>
2022-06-14 10:47:34 +0800

    '.'

---
+++ compare_speed.py
@@ -0,0 +1,14 @@
+import torch
+import models
+from time import time
+img = torch.randn(1, 3, 256, 256)
+net = models.MobileViT_S()
+img = img.cuda()
+net = net.cuda()
+for i in range(20):
+    out = net(img)
+tik = time()
+for i in range(1000):
+    out = net(img)
+tok = time()
+print((tok-tik)/1000)
