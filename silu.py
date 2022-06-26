35aa3d0c13746b809bea5c4f6c35906ad0edf7c7
supersyz <362871010@qq.com>
2022-06-26 16:33:40 +0800

    '.'

--- silu.py
+++ silu.py
@@ -3,12 +3,17 @@ import onnx_graphsurgeon as gs
 import numpy as np
 graph = gs.import_onnx(onnx.load("./mobilevit.onnx"))
 sigmoids =  [node for node in graph.nodes if node.op == 'Sigmoid']
+i = 0
 for sig in sigmoids:
-    silu_inputs = sig.inputs
+    i += 1
+    parent_node = sig.i()
+    parent_node.outputs = [gs.Variable(name='parent_output_'+str(i))]
+    silu_inputs = parent_node.outputs
     mul = sig.o()
     #print(mul)
     mul_outputs = mul.outputs
-    silu = gs.Node(op='SiLU',inputs=silu_inputs,outputs=mul_outputs)
+    silu_outputs = [gs.Variable(name='silu_output_'+str(i))]
+    silu = gs.Node(op='SiLU',inputs=silu_inputs,outputs=silu_outputs)
     graph.nodes.append(silu)
     print(mul.o())
     downstreams = [node for node in graph.nodes if mul_outputs[0] in node.inputs]
