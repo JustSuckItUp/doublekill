8efbb0240bec2727e81fa63ec2fd70c1716421f9
supersyz <362871010@qq.com>
2022-06-26 16:57:16 +0800

    '.'

--- silu.py
+++ silu.py
@@ -6,9 +6,9 @@ sig = [node for node in graph.nodes if node.name == 'Sigmoid_32'][0]
 p = sig.i()
 c = sig.o()
 g = c.o()
-children = [node for node in graph.nodes if c.outputs[0] in g.inputs]
+children = [node for node in graph.nodes if c.outputs[0] in node.inputs]
 
-print(p,c,g,sep='='*50)
+#print(p,c,g,sep='='*50)
 print(children)
 p.outputs = [gs.Variable(name='parent_output')]
 silu_outputs = [gs.Variable(name='silu_output')]
