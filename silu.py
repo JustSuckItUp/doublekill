8cece80d711335ded7ca50b40bdd74e0bf5b415e
supersyz <362871010@qq.com>
2022-06-26 16:19:33 +0800

    '.'

--- silu.py
+++ silu.py
@@ -11,10 +11,10 @@ for sig in sigmoids:
     silu = gs.Node(op='SiLU',inputs=silu_inputs,outputs=mul_outputs)
     graph.nodes.append(silu)
     print(mul.o())
-    # downstreams = [node for node in mul.o()]
-    # for ds in downstreams:
-    #     for i in range(len(ds.inputs)):
-    #         if ds.inputs[i] == mul_outputs[0]:
-    #             ds.inputs[i] = silu.outputs[0]
+    downstreams = [node for node in graph.nodes if mul_outputs[0] in node.inputs]
+    for ds in downstreams:
+        for i in range(len(ds.inputs)):
+            if ds.inputs[i] == mul_outputs[0]:
+                ds.inputs[i] = silu.outputs[0]
 graph.cleanup().toposort()
 onnx.save(gs.export_onnx(graph), "./mobilevit_silu.onnx")
