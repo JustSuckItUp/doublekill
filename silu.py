7d1e1c80e6b23c9627c1ba8576efa10d4069a657
supersyz <362871010@qq.com>
2022-06-26 17:02:49 +0800

    '.'

--- silu.py
+++ silu.py
@@ -2,22 +2,23 @@ import onnx
 import onnx_graphsurgeon as gs
 import numpy as np
 graph = gs.import_onnx(onnx.load("./mobilevit.onnx"))
-sig = [node for node in graph.nodes if node.name == 'Sigmoid_32'][0]
-p = sig.i()
-c = sig.o()
-g = c.o()
-children = [node for node in graph.nodes if c.outputs[0] in node.inputs]
+sigs = [node for node in graph.nodes if node.op == 'Sigmoid']
+for sig in sigs:
+    p = sig.i()
+    c = sig.o()
+    g = c.o()
+    children = [node for node in graph.nodes if c.outputs[0] in node.inputs]
 
-#print(p,c,g,sep='='*50)
-print(children)
-p.outputs = [gs.Variable(name='parent_output')]
-silu_outputs = [gs.Variable(name='silu_output')]
-silu = gs.Node(op='SiLU',inputs=p.outputs,outputs=silu_outputs)
-graph.nodes.append(silu)
-for child in children:
-    for j in range(len(child.inputs)):
-        if child.inputs[j] == c.outputs[0]:
-            child.inputs[j] = silu_outputs[0]
+    #print(p,c,g,sep='='*50)
+    print(children)
+    p.outputs = [gs.Variable(name='parent_output')]
+    silu_outputs = [gs.Variable(name='silu_output')]
+    silu = gs.Node(op='SiLU',inputs=p.outputs,outputs=silu_outputs)
+    graph.nodes.append(silu)
+    for child in children:
+        for j in range(len(child.inputs)):
+            if child.inputs[j] == c.outputs[0]:
+                child.inputs[j] = silu_outputs[0]
 #g.inputs[0] = silu_outputs[0]
 
 # sigmoids =  [node for node in graph.nodes if node.op == 'Sigmoid']
